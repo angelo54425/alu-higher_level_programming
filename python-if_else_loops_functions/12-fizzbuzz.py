71c0591251406312a9234f3a60959e62b12c3335
angelo54425 <e.gasana@alustudent.com>
2023-07-10 08:38:49 -0700

    13. Fizz Buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(101):
-        if x % 3 == 0 and x%5 == 0:
-            print(x, '')
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz" + " ")
         elif x % 3:
-            print(x, '')
+            print("Fizz" + " ")
         elif x % 5:
-            print(x, '')
+            print("Buzz" + " ")
         else:
-            print(x, '')
+            print("{}".format(x) + " ")
