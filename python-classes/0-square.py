e8499c657ae1f2f1ed49af7c4d34a417841d3a4f
angelo54425 <e.gasana@alustudent.com>
2023-07-25 13:52:53 -0700

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 class Square:
-"""Create an Empty Square """
-
-
-class Square:
-    ''' creates an empty square'''
     pass
+    """
+    creates a square class object
+    """
