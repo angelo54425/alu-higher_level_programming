e577eb8b8138018a7b29d7d0b65c29935e59e0ff
angelo54425 <e.gasana@alustudent.com>
2023-10-02 20:36:18 +0000

    3. Star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
+
 const request = require('request');
-const num = process.argv[2];
-request('http://swapi.co/api/films/' + num, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body)['title']);
-  }
-});
+
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(`An error occurred while making the request: ${error}`);
+    } else if (response.statusCode !== 200) {
+      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+    } else {
+      const movie = JSON.parse(body);
+      console.log(movie.title);
+    }
+  });
+} else {
+  console.log('Please provide the movie ID as an argument.');
+}
